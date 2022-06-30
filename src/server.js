aa452955302785298fbd420da6fdcab7801c4865
Matthew Rachid Romie Nascimento <matthew.nascimento@compasso.com.br>
2022-06-30 01:07:53 -0300

    Receive five olders repositories

---
+++ src/server.js
@@ -0,0 +1,40 @@
+const cors = require('cors')
+const express = require('express');
+const app = express();
+const axios = require('axios');
+
+app.use(cors())
+
+app.listen(3000,()=>{
+  console.log(3000)
+})
+
+app.get("/", async(req,res)=>{
+  // Todos os repositorios
+  let allRepositories = []
+  
+  // Requisição de todos os repositórios
+  for (let index = 1; index <= 4; index++) {
+  try{  
+    const {data} = await axios(`https://api.github.com/users/takenet/repos?page=${index}`)
+    allRepositories.push(...data)
+  } catch(error){ 
+    console.log(error)
+  }
+}
+
+//Filtro C#
+let CSharp = allRepositories.filter((repo)=>{
+  return repo.language === 'C#'
+})
+
+//Ordenação em data crescente
+let Order = CSharp.sort((a,b)=> new Date(a.created_at).valueOf() - new Date(b.created_at).valueOf())
+
+//Cinco repositorios mais velhos
+let FiveOlders = Order.slice(0,5)
+
+res.json(FiveOlders)
+
+
+})
