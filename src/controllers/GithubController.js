8079997d0e58098a43908bcf065c00c509f5bf34
Matthew Rachid Romie Nascimento <matthew.nascimento@compasso.com.br>
2022-07-03 20:23:00 -0300

    folder utils

--- src/controllers/GithubController.js
+++ src/controllers/GithubController.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
+import Utils from '../utils'
 
 class GithubController{
   async getRepositories(req,res){
     let utils = new Utils();
-    
     let arrayRepositories = [];
     for (let index = 1; index <= 4; index++) {
     try{  
@@ -12,7 +12,7 @@ class GithubController{
     } catch(error){ 
       res.status(400).send(error)
     }
-  }
+    }
     arrayRepositories = utils.filterByCSharp(arrayRepositories)
     arrayRepositories = utils.sortByData(arrayRepositories)
     arrayRepositories = utils.fiveOlders(arrayRepositories)
@@ -21,24 +21,5 @@ class GithubController{
   }
 }
 
-export class Utils{
-  filterByCSharp(repositories){
-    const language = "C#"
-
-    //Filtro C#
-    let CSharp = repositories.filter((repo)=>{
-      return repo.language === language
-    })
-    return CSharp
-  }
-  sortByData(repositories){
-    let Order = repositories.sort((a,b)=> new Date(a.created_at).valueOf() - new Date(b.created_at).valueOf())
-    return Order
-  }
-  fiveOlders(repositories){
-    let FiveOlders = repositories.slice(0,5)
-    return FiveOlders
-  }
-}
 
 export default new GithubController();
