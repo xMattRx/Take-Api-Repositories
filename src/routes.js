362f58ed09dd2a60d0c44d26444af634122289f4
Matthew Rachid Romie Nascimento <matthew.nascimento@compasso.com.br>
2022-07-03 22:56:49 -0300

    trying fix

--- src/routes.js
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import { GithubController } from './controllers';
-
-const routes = new Router()
-
-routes.get('/', GithubController.getRepositories)
-
-export default routes;
--- src/server.js
+++
@@ -1,7 +0,0 @@
-
-import app from './app'
-
-app.listen(process.env.PORT || 5000,()=>{
-  console.log(5000)
-})
-
